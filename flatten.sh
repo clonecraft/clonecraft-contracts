bb911f5fc50cfb83b4003ca3dbeac57817115b88
elixirevo <elixirevocation@gmail.com>
2023-03-23 14:59:57 +0900

    update: flatten

---
+++ flatten.sh
@@ -0,0 +1,6 @@
+npx hardhat flatten contracts/v3/ClonesneverdieV3.sol > ./flatten/ClonesneverdieV3.sol
+npx hardhat flatten contracts/asset/ClonesneverdieAsset.sol > ./flatten/ClonesneverdieAsset.sol
+npx hardhat flatten contracts/v3-sale/V3Sale.sol > ./flatten/V3Sale.sol
+npx hardhat flatten contracts/v3-level/LevelToken.sol > ./flatten/LevelToken.sol
+npx hardhat flatten contracts/v3-level/V3LevelStorage.sol > ./flatten/V3LevelStorage.sol
+npx hardhat flatten contracts/v3-level/V3LevelUp.sol > ./flatten/V3LevelUp.sol
